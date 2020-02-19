a386a998a7830afc53d6a4a130b3905cbec73d85
Jonatas Santos <jonatasricardo90@gmail.com>
2020-02-18 23:04:03 -0300

    aula 3.2

--- src/js/vtex-dev.js
+++ src/js/vtex-dev.js
@@ -1,8 +1,10 @@
 // Home
-$('.vtex__content-place-holder--bannerHome').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true
-});
+if($("body").hasClass("home")) {
+    $('.home .vtex__content-place-holder--bannerHome').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+}
