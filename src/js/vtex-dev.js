fd06a5a27f1178027e1935e1bb1726dc0a2c6bfb
Jonatas Santos <jonatasricardo90@gmail.com>
2020-02-24 16:51:10 -0300

    aula 3.5

--- src/js/vtex-dev.js
+++ src/js/vtex-dev.js
@@ -18,43 +18,15 @@ if ($('body').hasClass('catalog')) {
 		filtersMenu:".search-multiple-navigator", // Menu com os filtros
 		linksMenu:".search-single-navigator", // Menu de links
 		menuDepartament:".vtex__search-navigator .menu-departamento", // seletor do menu da página de departamentos
-		mergeMenu:true, // Define se o menu de links será mesclado com o de filtros será mesclado na página de departamento
-		insertMenuAfter:".search-multiple-navigator h3:first", // O menu de links será inserido após este elemento
-		emptySearchElem:jQuery('<div class="vtexsr-emptySearch"></div>'), // Elemento Html (em Objeto jQuery) no qual será adicionado a mensagem de busca vazia
-		elemLoading:'<div id="scrollLoading">Carregando ... </div>', // Elemento com mensagem de carregando ao iniciar a requisição da página seguinte
-		returnTopText:'<span class="text">voltar ao</span><span class="text2">TOPO</span>', // Mensagem de "retornar ao topo"
-		emptySearchMsg:'<h3>Esta combinação de filtros não retornou nenhum resultado!</h3>', // Html com a mensagem para ser apresentada quando não existirem resultados para os filtros selecionados
-		filterErrorMsg:"Houve um erro ao tentar filtrar a página!", // Mensagem de erro exibida quando existe algum erro de servidor ao aplicar os filtros
-		searchUrl:null, // Url da página de busca (opicional)
-		usePopup:false, // Opção p/ definir se deseja que a mensagem de não localizado seja exibida em um popup
-		showLinks:true, // Exibe o menu de links caso o de filtro não seja encontrado
-		popupAutoCloseSeconds:3, // Caso esteja utilizando popup, defina aqui o tempo para que ele feche automaticamente
-		// Função que retorna o valor p/ onde a página deve rolar quando o usuário marca ou desmarca um filtro
-		filterScrollTop:function(shelfOffset)
-		{
-			return (shelfOffset.top-20);
-		},
-		callback:function(){},
-		// Cálculo do tamanho do conteúdo/vitrine para que uma nova página seja chamada antes do usuário chegar ao "final" do site
-		getShelfHeight:function(container)
-		{
-			return (container.scrollTop()+container.height());
-		},
-		// Callback após inserir a prateleira na página
-		shelfCallback:function(){},
-		// Callback em cada requisição Ajax (Para requisições feitas com sucesso)
-		// Recebe como parâmetro um objeto contendo a quantidade total de requisições feitas e a quantidade de filtros selecionados
-		ajaxCallback:function(){},
-		// Função que é executada quando a seleção de filtros não retorna nenhum resultado
-		// Recebe como parâmetro um objeto contendo a quantidade total de requisições feitas e a quantidade de filtros selecionados
-		emptySearchCallback:function(){},
-		// Função para permitir ou não que a rolagem infinita execute na página esta deve retornar "true" ou "false"
-		// Recebe como parâmetro um objeto contendo a quantidade total de requisições feitas e a quantidade de filtros selecionados
-		authorizeScroll:function(){return true;},
-		// Função para permitir ou não que o conteúdo de "loadContent" seja atualizado. Esta deve retornar "true" ou "false"
-		// Recebe como parâmetro um objeto contendo a quantidade total de requisições feitas e a quantidade de filtros selecionados
-		authorizeUpdate:function(){return true;},
-		// Callback de cada laço percorrendo os fildsets e os labels. Retorna um objeto com algumas informações
-		labelCallback:function(data){}
     });
+}
+
+// Catalog - Search
+if($('body').hasClass('catalog--search')) {
+	function searchTerm() {
+		var term = vtxctx.searchTerm;
+		var qty =  $('.resultado-busca-numero .value').first().text();
+		$('.vtex__search-title').append('<h2 class="search-term"> '+qty+' resultados para <strong> '+ term +'</strong></h2>');
+	}
+	searchTerm();
 }
